40a005a11ebaf13721a51a94219e7daf8a484270
sheep3123 <100734911+sheep3123@users.noreply.github.com>
2022-03-03 15:35:41 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
     alert("点击了图片");
-   targetImage.classList.remove("small");
+        if (targetImage.classList.contains("small")) {
+    targetImage.classList.remove("small");
+} else {
+    targetImage.classList.add("small");
+}
+});
 });
